4fb135d05bd83e007bcad4df50a53e0290104278
shubham <tiwarishubh723@gmail.com>
2026-05-22 15:37:33 +0530

    fix prisma vercel deployment

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import express from "express";
 import { connectDB } from "./db.js";
 import authRouter from "./src/routes/authRuth.js";
 import cookieParser from "cookie-parser";
 import path from "path";
 import expensesRouter from "./src/routes/expense.js";
+
 const app = express();
-const PORT = 5000;
 const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
@@ -20,17 +22,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+await connectDB();
 
-const startServer = async () => {
-    try {
-        await connectDB();
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.error("Failed to start server:", error);
-        process.exit(1);
-    }
-};
-
-startServer();
+export default app;
